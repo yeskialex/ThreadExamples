3fd136477dfe1708f6b3ec6eff9adcc625e4e382
JC <jc@lifove.net>
2018-06-04 02:11:58 +0900

    Minor change

--- src/main/java/edu/handong/csee/java/examples/thread/lab17/SumMultipleThreads.java
+++ src/main/java/edu/handong/csee/java/examples/thread/lab17/SumMultipleThreads.java
@@ -6,24 +6,11 @@ public class SumMultipleThreads implements Runnable{
 
 	long from, to, totalSum;
 
-	public SumMultipleThreads(long from, long to) {
-		this.from = from;
-		this.to= to;
-	}
-
-	public long getTotalSumFromTo() {
-		long total = 0;
-		for(long i=from;i<=to;i++)
-			total+=i;
-		
-		return total;
-	}
-
 	public static void main(String[] args) {
-		
+
 		long to = 10000000;
 		ArrayList<SumMultipleThreads> sumRunners = new ArrayList<SumMultipleThreads>();
-		
+
 		/* Let a thread compute a sub-sum.
 		 * sumRunners.add(new SumMultipleThreads(1,1000000));
 		 * sumRunners.add(new SumMultipleThreads(1000001,2000000));
@@ -38,20 +25,20 @@ public class SumMultipleThreads implements Runnable{
 		for(long i=0; i<to/1000000; i++) {
 			sumRunners.add(new SumMultipleThreads((i*1000000)+1, (i+1)*1000000));
 		}
-		
+
 		ArrayList<Thread> threadsForSubSum = new ArrayList<Thread>();
-		
+
 		for(SumMultipleThreads runner:sumRunners) {
 			Thread thread = new Thread(runner);
 			thread.start();
 			threadsForSubSum.add(thread);
 		}
-		
+
 		long grandTotal = 0;
 		for(SumMultipleThreads runner:sumRunners) {
 			grandTotal += runner.totalSum;
 		}
-		
+
 		System.out.println("Grand Total = " + grandTotal);
 	}
 
@@ -59,5 +46,18 @@ public class SumMultipleThreads implements Runnable{
 	public void run() {
 		totalSum = getTotalSumFromTo();
 		System.out.println("Computed from " + from + " to " + to);
+	}
+
+	public SumMultipleThreads(long from, long to) {
+		this.from = from;
+		this.to= to;
+	}
+
+	public long getTotalSumFromTo() {
+		long total = 0;
+		for(long i=from;i<=to;i++)
+			total+=i;
+
+		return total;
 	}
 }
