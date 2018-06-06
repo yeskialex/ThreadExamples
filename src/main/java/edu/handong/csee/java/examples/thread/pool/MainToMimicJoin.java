6a70101fbb8043ac26977b86bc062f478c3f9537
JC <JC jc@lifove.net>
2018-06-06 23:21:41 +0900

    ADd thread pool examples

---
+++ src/main/java/edu/handong/csee/java/examples/thread/pool/MainToMimicJoin.java
@@ -0,0 +1,46 @@
+package edu.handong.csee.java.examples.thread.pool;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainToMimicJoin {
+
+	public static void main(String[] args) {
+		MainToMimicJoin runner = new MainToMimicJoin();
+		runner.doMyJob();
+	}
+
+	private void doMyJob() {
+		long to = 10000000;
+		ArrayList<SumPartiallyThread> sumRunners = new ArrayList<SumPartiallyThread>();
+
+		int numOfCoresInMyCPU = Runtime.getRuntime().availableProcessors();
+		System.out.println("The number of cores of my system: " + numOfCoresInMyCPU);
+		
+		ExecutorService executor = Executors.newFixedThreadPool(numOfCoresInMyCPU);
+
+		ArrayList<Callable<Object>> calls = new ArrayList<Callable<Object>>();
+		for(long i=0; i<to/1000000; i++) {
+			Runnable worker = new SumPartiallyThread((i*1000000)+1, (i+1)*1000000);
+			executor.execute(worker);
+			sumRunners.add((SumPartiallyThread)worker);
+			calls.add(Executors.callable(new SumPartiallyThread((i*1000000)+1, (i+1)*1000000)));
+		}
+		
+		try {
+			executor.invokeAll(calls); // This line will be terminated after all threads are terminated.
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		long grandTotal = 0;
+		for(SumPartiallyThread runner:sumRunners) {
+			grandTotal += runner.totalSum;
+		}
+		
+		System.out.println("Grand Total = " + grandTotal);
+	}
+
+}
---
+++ src/main/java/edu/handong/csee/java/examples/thread/pool/SumPartiallyThread.java
@@ -0,0 +1,25 @@
+package edu.handong.csee.java.examples.thread.pool;
+
+public class SumPartiallyThread implements Runnable{
+
+	long from, to, totalSum;
+	
+	@Override
+	public void run() {
+		totalSum = getTotalSumFromTo();
+		System.out.println("Computed from " + from + " to " + to);
+	}
+
+	public SumPartiallyThread(long from, long to) {
+		this.from = from;
+		this.to= to;
+	}
+
+	public long getTotalSumFromTo() {
+		long total = 0;
+		for(long i=from;i<=to;i++)
+			total+=i;
+
+		return total;
+	}
+}
