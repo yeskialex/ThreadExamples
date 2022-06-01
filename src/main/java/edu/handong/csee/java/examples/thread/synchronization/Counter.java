a4f68b431c99155de503525b74049da7befb7c4d
JC <jcnam@handong.edu>
2022-06-02 08:58:09 +0900

    Add messages

--- src/main/java/edu/handong/csee/java/examples/thread/synchronization/Counter.java
+++ src/main/java/edu/handong/csee/java/examples/thread/synchronization/Counter.java
@@ -3,9 +3,13 @@ package edu.handong.csee.java.examples.thread.synchronization;
 public class Counter {
 	private int c = 0;
     public void RunIncrease(String threadName) {
-    	System.out.println("The " + threadName + "'s counter is " + getValue() + " and is increasing.");
-        increase();
-        System.out.println("The " + threadName + "'s counter value is now: " + getValue() );
+    	//synchronized(this) {
+	    System.out.println("The " + threadName + "'s counter is " + getValue() + " and is increasing.");
+	    increase();
+	    System.out.println("The " + threadName + "'s counter value is now: " + getValue() );
+    	//}
+        System.out.println("The " + threadName + "'s counter value is again: " + getValue() );
+        
     }
     
     public void increase() {
@@ -13,9 +17,12 @@ public class Counter {
     }
     
     public void runDecrease(String threadName) {
-    	System.out.println("The " + threadName + "'s counter is " + getValue() + " and is decreasing.");
-        decrease();
-        System.out.println("The " + threadName + "'s counter value is now: " + getValue() );
+    	//synchronized(this) {
+	    System.out.println("The " + threadName + "'s counter is " + getValue() + " and is decreasing.");
+	    decrease();
+	    System.out.println("The " + threadName + "'s counter value is now: " + getValue() );
+    	//}
+        System.out.println("The " + threadName + "'s counter value is again: " + getValue() );
     }
     
     public void decrease() {
