313d53fb25e65be2d55c6eb3a155f338e1af192a
yeskialex <yeskialex@gmail.com>
2023-06-08 14:37:32 +0900

    commit

--- src/main/java/edu/handong/csee/java/examples/thread/lab/SumMultipleThreads.java
+++ src/main/java/edu/handong/csee/java/examples/thread/lab/SumMultipleThreads.java
@@ -21,22 +21,32 @@ public class SumMultipleThreads {
 		 * sumRunners.add(new SumMultipleThreads(7000001,8000000));
 		 * sumRunners.add(new SumMultipleThreads(8000001,9000000));
 		 * sumRunners.add(new SumMultipleThreads(9000001,10000000));*/
-		for(long i=0; i<to/1000000; i++) {
-			SumRunner currentRunner = new SumRunner((i*1000000)+1, (i+1)*1000000);
-			sumRunners.add(currentRunner);
-			
-			Thread thread = new Thread(currentRunner);
-			thread.start();
-			threadsForSubSum.add(thread);
-			System.out.println("Thread-" + i + " started!");
+		for (long i = 0; i < to / 1000000; i++) {
+		    SumRunner currentRunner = new SumRunner((i * 1000000) + 1, (i + 1) * 1000000);
+		    sumRunners.add(currentRunner);
+
+		    Thread thread = new Thread(currentRunner);
+		    thread.start();
+		    threadsForSubSum.add(thread);
+		    System.out.println("Thread-" + i + " started!");
+		}
+
+		// Wait for each thread to complete in a random order
+		for (Thread thread : threadsForSubSum) {
+		    try {
+		        thread.join();
+		    } catch (InterruptedException e) {
+		        e.printStackTrace();
+		    }
 		}
 
 		long grandTotal = 0;
-		for(SumRunner runner:sumRunners) {
-			grandTotal += runner.totalSum;
+		for (SumRunner runner : sumRunners) {
+		    grandTotal += runner.totalSum;
 		}
 
 		System.out.println("Grand Total = " + grandTotal);
+
 	}
 
 }
